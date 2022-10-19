ae2a1c1317c122de3d94e55e127ae93a6cbc1a17
Floyd Furtado <113371065+Floyd-Furtado@users.noreply.github.com>
2022-10-19 07:10:55 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("My first console project");
+var first_name = "Floyd";
+var last_name = "Furtado";
+
+var full_name = first_name.concat(last_name) ;
+console.log(full_name);
+
+var a = 88;
+var b = 12;
+
+
+var add = a + b  ;
+console.log(add);
+
+var sub = a - b  ;
+console.log(sub);
+
+
+var multiple = a * b  ;
+console.log(multiple);
+
+var divide = a / b ;
+console.log(divide);
